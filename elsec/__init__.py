9a54e93fa68acf76bcdef253fd1c43c9f9e9b5ba
Mik Kocikowski <mkocikowski@gmail.com>
2013-02-02 14:07:08 -0800

    Top level docstring in elsec.__init__

--- elsec/__init__.py
+++ elsec/__init__.py
@@ -1,1 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""Interactive command line application for querying Elasticsearch. 
+
+Modules:
+- client.py: main() is the entry point for the application, main input loop
+- parser.py: provides tab completion, execute() parses and executes commands
+- actions.py: calls to Elasticsearch, used by parser.py
+- http.py: wrappers around httplib, used by actions.py for http calls
+- help.py: help strings
+- exceptions.py: exceptions
+- templates.py: JSON templates for Elasticsearch requests
+
+Tests:
+All tests are in the 'test' package. The test runner is in 'units.py'. For
+tests to run there needs to be an Elasticsearch server running on localhost. 
+
+"""
+
+
 __version__ = "0.0.3"
